110f0382491f44d82d498e0aeb7b58c2eb683e2b
fanyangjeff <fanyangjeff1148@gmail.com>
2021-09-19 10:31:02 -0700

    follow & unfollow

--- src/main/java/com/twitter/controller/UserController.java
+++ src/main/java/com/twitter/controller/UserController.java
@@ -13,13 +13,13 @@ public class UserController {
         this.userService = userService;
     }
 
-    @RequestMapping(path = "/follow", method = {RequestMethod.GET, RequestMethod.PUT})
+    @RequestMapping(path = "/follow", method = {RequestMethod.GET, RequestMethod.POST})
     public void follow(@RequestParam String followerId, @RequestParam String followeeId) {
-
+        userService.follow(Long.parseLong(followerId), Long.parseLong(followeeId));
     }
 
-    @RequestMapping(path = "/unfollow", method = {RequestMethod.GET, RequestMethod.PUT})
+    @RequestMapping(path = "/unfollow", method = {RequestMethod.GET, RequestMethod.POST})
     public void unfollow(@RequestParam String followerId, @RequestParam String followeeId) {
-
+        userService.unfollow(Long.parseLong(followerId), Long.parseLong(followeeId));
     }
 }
