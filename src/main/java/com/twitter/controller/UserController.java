b67ee7685b506651468ffb85699c3fc3b6d73e22
fanyangjeff <fanyangjeff1148@gmail.com>
2021-09-18 20:02:29 -0700

    edited follow and unfollow

--- src/main/java/com/twitter/controller/UserController.java
+++ src/main/java/com/twitter/controller/UserController.java
@@ -1,4 +1,25 @@
 package com.twitter.controller;
 
+import com.twitter.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/user")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(@Autowired UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping(path = "/follow", method = {RequestMethod.GET, RequestMethod.PUT})
+    public void follow(@RequestParam String followerId, @RequestParam String followeeId) {
+
+    }
+
+    @RequestMapping(path = "/unfollow", method = {RequestMethod.GET, RequestMethod.PUT})
+    public void unfollow(@RequestParam String followerId, @RequestParam String followeeId) {
+
+    }
 }
--- src/main/java/com/twitter/service/UserService.java
+++ src/main/java/com/twitter/service/UserService.java
@@ -1,7 +1,24 @@
 package com.twitter.service;
 
+import com.twitter.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private final UserDao userDao;
+
+    public UserService(@Autowired UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void follow(long followerId, long followeeId) {
+        System.out.println(followerId);
+        System.out.println(followeeId);
+    }
+
+    public void unfollow(long followerId, long followeeId) {
+        System.out.println(followerId);
+        System.out.println(followeeId);
+    }
 }
